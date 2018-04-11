47ef402c71df1c795abc849a635dd8fca9552e3a
Olena Hedz <Lenka-949@mail.ru>
2018-04-11 09:24:48 +0300

    Update SqlImpl.java

--- src/main/java/ua/training/db/SqlImpl.java
+++ src/main/java/ua/training/db/SqlImpl.java
@@ -33,19 +33,19 @@ public class SqlImpl {
         ON UPDATE NO ACTION;
         ADD UNIQUE INDEX `UN_DRIVER_CAR` (`Car_id` ASC, `Driver_id` ASC);
 
-        Заполняем таблицу с машинами:
+        //Заполняем таблицу с машинами:
         insert into car(model, manufacture) values('Focus', 'Ford');
         insert into car(model, manufacture) values('Clio', 'Reno');
         insert into car(model, manufacture) values('Toledo', 'Seat');
-        Заполняем таблицу с водителями:
+        //Заполняем таблицу с водителями:
         insert into driver(name, telephone) values('Ivan Popov', '+380665201477');
         insert into driver(name, telephone) values('Petr Sergienko', '+380503125896');
         insert into driver(name, telephone) values('Nina Apina', '+380936658815');
         insert into driver(name, telephone) values('David Orlov', '+380446178455');
-        Заполняем связующую таблицу:
+        //Заполняем связующую таблицу:
         insert into cardriver(Car_id, Driver_id) values(1, 2);
         insert into cardriver(Car_id, Driver_id) values(2, 1);
-        Проверяем всязи:
+        //Проверяем всязи:
         select *
                 from cardriver cd
         left join car c on c.id = cd.Car_id
